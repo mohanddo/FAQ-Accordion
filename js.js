2e41ab7d6a70ee5fa796702637bf03720b4984af
mohanddo <93376862+mohanddo@users.noreply.github.com>
2023-12-12 18:59:25 +0000

    First submit

---
+++ js.js
@@ -0,0 +1,57 @@
+let hideANDshowElement0 = () => {
+    let element = document.querySelectorAll("p:only-child")[0];
+    element.classList.toggle("hide");   
+    
+    if(paths[0].getAttribute("d") == "M15 3.313A12.187 12.187 0 1 0 27.188 15.5 12.203 12.203 0 0 0 15 3.312Zm4.688 13.124h-3.75v3.75a.938.938 0 0 1-1.876 0v-3.75h-3.75a.938.938 0 0 1 0-1.875h3.75v-3.75a.938.938 0 0 1 1.876 0v3.75h3.75a.938.938 0 0 1 0 1.876Z") {
+        paths[0].setAttribute("d", "M15 3.313A12.187 12.187 0 1 0 27.188 15.5 12.2 12.2 0 0 0 15 3.312Zm4.688 13.124h-9.375a.938.938 0 0 1 0-1.875h9.374a.938.938 0 0 1 0 1.876Z")
+    } else {
+        paths[0].setAttribute("d", "M15 3.313A12.187 12.187 0 1 0 27.188 15.5 12.203 12.203 0 0 0 15 3.312Zm4.688 13.124h-3.75v3.75a.938.938 0 0 1-1.876 0v-3.75h-3.75a.938.938 0 0 1 0-1.875h3.75v-3.75a.938.938 0 0 1 1.876 0v3.75h3.75a.938.938 0 0 1 0 1.876Z")
+    }    
+};
+
+let hideANDshowElement1 = () => {
+    let element = document.querySelectorAll("p:only-child")[1];
+    element.classList.toggle("hide");   
+    
+    if(paths[1].getAttribute("d") == "M15 3.313A12.187 12.187 0 1 0 27.188 15.5 12.203 12.203 0 0 0 15 3.312Zm4.688 13.124h-3.75v3.75a.938.938 0 0 1-1.876 0v-3.75h-3.75a.938.938 0 0 1 0-1.875h3.75v-3.75a.938.938 0 0 1 1.876 0v3.75h3.75a.938.938 0 0 1 0 1.876Z") {
+        paths[1].setAttribute("d", "M15 3.313A12.187 12.187 0 1 0 27.188 15.5 12.2 12.2 0 0 0 15 3.312Zm4.688 13.124h-9.375a.938.938 0 0 1 0-1.875h9.374a.938.938 0 0 1 0 1.876Z")
+    } else {
+        paths[1].setAttribute("d", "M15 3.313A12.187 12.187 0 1 0 27.188 15.5 12.203 12.203 0 0 0 15 3.312Zm4.688 13.124h-3.75v3.75a.938.938 0 0 1-1.876 0v-3.75h-3.75a.938.938 0 0 1 0-1.875h3.75v-3.75a.938.938 0 0 1 1.876 0v3.75h3.75a.938.938 0 0 1 0 1.876Z")
+    }    
+};
+
+let hideANDshowElement2 = () => {
+    let element = document.querySelectorAll("p:only-child")[2];
+    element.classList.toggle("hide");   
+    
+    if(paths[2].getAttribute("d") == "M15 3.313A12.187 12.187 0 1 0 27.188 15.5 12.203 12.203 0 0 0 15 3.312Zm4.688 13.124h-3.75v3.75a.938.938 0 0 1-1.876 0v-3.75h-3.75a.938.938 0 0 1 0-1.875h3.75v-3.75a.938.938 0 0 1 1.876 0v3.75h3.75a.938.938 0 0 1 0 1.876Z") {
+        paths[2].setAttribute("d", "M15 3.313A12.187 12.187 0 1 0 27.188 15.5 12.2 12.2 0 0 0 15 3.312Zm4.688 13.124h-9.375a.938.938 0 0 1 0-1.875h9.374a.938.938 0 0 1 0 1.876Z")
+    } else {
+        paths[2].setAttribute("d", "M15 3.313A12.187 12.187 0 1 0 27.188 15.5 12.203 12.203 0 0 0 15 3.312Zm4.688 13.124h-3.75v3.75a.938.938 0 0 1-1.876 0v-3.75h-3.75a.938.938 0 0 1 0-1.875h3.75v-3.75a.938.938 0 0 1 1.876 0v3.75h3.75a.938.938 0 0 1 0 1.876Z")
+    }    
+};
+
+let hideANDshowElement3 = () => {
+    let element = document.querySelectorAll("p:only-child")[3];
+    element.classList.toggle("hide");   
+    
+    if(paths[3].getAttribute("d") == "M15 3.313A12.187 12.187 0 1 0 27.188 15.5 12.203 12.203 0 0 0 15 3.312Zm4.688 13.124h-3.75v3.75a.938.938 0 0 1-1.876 0v-3.75h-3.75a.938.938 0 0 1 0-1.875h3.75v-3.75a.938.938 0 0 1 1.876 0v3.75h3.75a.938.938 0 0 1 0 1.876Z") {
+        paths[3].setAttribute("d", "M15 3.313A12.187 12.187 0 1 0 27.188 15.5 12.2 12.2 0 0 0 15 3.312Zm4.688 13.124h-9.375a.938.938 0 0 1 0-1.875h9.374a.938.938 0 0 1 0 1.876Z")
+    } else {
+        paths[3].setAttribute("d", "M15 3.313A12.187 12.187 0 1 0 27.188 15.5 12.203 12.203 0 0 0 15 3.312Zm4.688 13.124h-3.75v3.75a.938.938 0 0 1-1.876 0v-3.75h-3.75a.938.938 0 0 1 0-1.875h3.75v-3.75a.938.938 0 0 1 1.876 0v3.75h3.75a.938.938 0 0 1 0 1.876Z")
+    }    
+};
+
+
+let iconPlusMinus = document.querySelectorAll("svg");
+let paths = document.querySelectorAll(".path")
+
+iconPlusMinus[2].addEventListener("click", hideANDshowElement0);
+
+iconPlusMinus[3].addEventListener("click", hideANDshowElement1);
+
+iconPlusMinus[4].addEventListener("click", hideANDshowElement2);
+
+iconPlusMinus[5].addEventListener("click", hideANDshowElement3);
+
+console.log(window.innerWidth)
